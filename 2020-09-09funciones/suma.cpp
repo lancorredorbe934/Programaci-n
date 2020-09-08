bf074fd3e3305f97d11db97c4665c48e9eefd2a7
Laura Corredor <lancorredorbe@unal.edu.co>
2020-09-08 12:22:08 -0500

    Se crea còdigo con ejemplo de una funcion que suma los nùmeros divisibles entre 3,5 o 7

---
+++ 2020-09-09funciones/suma.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int suma(int m, int n); 
+
+int main(void)
+{
+  std::cout<< suma(1, 10) <<std::endl; 
+  return 0;
+}
+
+int suma(int m, int n)
+{
+  int suma=0;
+  for(int ii=m; ii<=n; ++ii)
+  {
+    if((ii%5==0) or (ii%7==0) or (ii%3==0)){
+    suma +=ii;
+    }
+  }
+  
+  return suma; 
+}
